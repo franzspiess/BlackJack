1d1ff281cb204b19a338eda67a419d55a3fed2ea
PinchePanchoPincho <franz.spiess@gmail.com>
2018-07-27 15:06:32 +0200

    Sean2

--- blackjack2.js
+++ blackjack2.js
@@ -7,11 +7,17 @@ var y = 2;
 var z = 2;
 var card0 = 'card0';
 var card1 = 'card1';
+var count = 0;
 
 function reset() {
-    deck = [];
+    $('.cards0').children('.card').remove();
+    $('.cards1').children('.card').remove();
+    deck.length = 0;
     points0 = 0;
     points1 = 0;
+    y = 2;
+    z = 2;
+    count = 0;
 }
 
 function createDeck() {
@@ -50,16 +56,21 @@ function dealHands() {
     for (let i = 0; i<2; i++) {
         for (let x = 0; x<2; x++) {
             let card = deck.pop();
+            if (card.weight = 11){
+                players[i].AceCount +=1
+            } 
             players[i].Hand.push(card);
-            //updateDeck();
-            // renderCard();
+            
             // updateDeck();
-            // updatePoints();
+            //updatePoints();
         }
     }
+    updatePoints();
+    updateDeck();
+    renderCard();
+    
     //updatePoints();
     //renderCard();
-
 }
 
 function updateDeck()
@@ -70,7 +81,7 @@ function updateDeck()
 var hand1 = [];
 var hand2; [];
 
-var players = [{Name: 'Player1', ID: 1, Points: 0, Hand: []},{Name: 'House', ID:2, Points: 0, Hand: []}];
+var players = [{Name: 'Player1', ID: 1, Points: 0, Hand: [], AceCount: 0},{Name: 'House', ID:2, Points: 0, Hand: [], AceCount: 0}];
 
 /* function createPlayersUI()
 {
@@ -94,36 +105,49 @@ var players = [{Name: 'Player1', ID: 1, Points: 0, Hand: []},{Name: 'House', ID:
 } */
 
 function renderCard() {
-    var el = document.createElement('div');
+    /*var el = document.createElement('div');
         el.className = 'card';
-        el.innerHTML = players[0].Hand[1].suit + " " + players[0].Hand[1].value;
-        game0.appendChild(el)
+        el.innerHTML = players[0].Hand[0].suit + " " + players[0].Hand[0].value;
+        cards.appendChild(el)*/
+        for (let i = 0; i < 2; i++) {
+            $( ".cards0" ).append( "<div class='card'>"+players[0].Hand[i].suit + " " + players[0].Hand[i].value+"</div>");
+        };
+        
+        for (let i = 0; i < 2; i++) {
+                $( ".cards1" ).append( "<div class='card'>"+players[1].Hand[i].suit + " " + players[1].Hand[i].value+"</div>");
+        };
+        /* $( ".cards" ).append( "<div class='card'>"+players[0].Hand[0].suit + " " + players[0].Hand[0].value+"</div>");
+        $( ".cards" ).append( "<div class='card'>"+players[0].Hand[0].suit + " " + players[0].Hand[0].value+"</div>");
+        $( ".cards" ).append( "<div class='card'>"+players[0].Hand[0].suit + " " + players[0].Hand[0].value+"</div>");
+        $( ".cards" ).append( "<div class='card'>"+players[0].Hand[0].suit + " " + players[0].Hand[0].value+"</div>");*/    
     //document.getElementById("card00").innerHTML = players[0].Hand[0].suit + " " + players[0].Hand[0].value;
     //document.getElementById("card01").innerHTML = players[0].Hand[1].suit + " " + players[0].Hand[1].value;// + '        Points: ' + players[0].Hand[1].weight;
-    document.getElementById("score0").innerHTML = points0;
-    document.getElementById("card10").innerHTML = players[1].Hand[0].suit + " " + players[1].Hand[0].value;// + '        Points: ' + players[1].Hand[0].weight;
-    document.getElementById("card11").innerHTML = players[1].Hand[1].suit + " " + players[1].Hand[1].value;
-    document.getElementById("score1").innerHTML = points1;
+    //document.getElementById("card10").innerHTML = players[1].Hand[0].suit + " " + players[1].Hand[0].value;// + '        Points: ' + players[1].Hand[0].weight;
+    //document.getElementById("card11").innerHTML = players[1].Hand[1].suit + " " + players[1].Hand[1].value;
+    $("#score0").html("YOUR SCORE: " + points0);
+    $("#score1").html("HOUSE SCORE: " + points1);
+    /*document.getElementById("score0").innerHTML = points0;
+    document.getElementById("score1").innerHTML = points1; */
 }
 /* */ 
 function start() {
     reset();
     createDeck();
     shuffle();
-    updateDeck();
+    //updateDeck();
     dealHands();
-    updatePoints();
-    renderCard();   ;
+    //updatePoints();
+    //renderCard();
 }
 
 
  function updatePoints() {
         for (let x=0; x<2; x++) {
             points0 += players[0].Hand[x].weight;}
-            console.log(points0);
+            // console.log(points0);
         for (let x=0; x<2; x++) {
             points1 += players[1].Hand[x].weight;}
-            console.log(points1);
+            //console.log(points1);
 } 
 
 dealHands();
@@ -131,26 +155,66 @@ updatePoints();
 
 function hitMe() {
     let card = deck.pop();
+    updateDeck();
     players[0].Hand.push(card);
-    document.getElementById(card0 + y).innerHTML = players[0].Hand[y].suit + " " + players[0].Hand[y].value;
+    //document.getElementById(card0 + y).innerHTML = players[0].Hand[y].suit + " " + players[0].Hand[y].value;
+    $( ".cards0" ).append( "<div class='card'>"+players[0].Hand[y].suit + " " + players[0].Hand[y].value+"</div>");
     points0 += players[0].Hand[y].weight;
-    document.getElementById("score0").innerHTML = points0;
+    //document.getElementById("score0").innerHTML = points0;
+    $("#score0").html("YOUR SCORE: " + points0);
     y++;
     if (points0 > 21) {
-        document.getElementById("result0").innerHTML = 'YOU LOSE!'
+        $("#result0").html("PLAYER LOSES");
+        //document.getElementById("result0").innerHTML = 'YOU LOSE!'
     }
     
 }
 
 function stay() {
+    setTimeout(stayInner(), 5000);
+}
+
+function stayInner() {
     while (points1 <= 21) {
+        if (points1 >= 17) {
+            break;
+        }
         let card = deck.pop();
+        updateDeck();
         players[0].Hand.push(card);
-        document.getElementById(card1 + z).innerHTML = players[1].Hand[z].suit + " " + players[1].Hand[z].value;
+        //document.getElementById(card1 + z).innerHTML = players[1].Hand[z].suit + " " + players[1].Hand[z].value;
+        $( ".cards1" ).append( "<div class='card'>"+players[0].Hand[z].suit + " " + players[0].Hand[z].value+"</div>");
         points1 += players[1].Hand[z].weight;
-        document.getElementById("score1").innerHTML = points1;
+        //document.getElementById("score1").innerHTML = points1;
+        $("#score1").html("YOUR SCORE: " + points1);
         z++;
     }
-    document.getElementById("result1").innerHTML = 'HOUSE LOSES!'
+    if (points0 <21 && points1 > 21) {
+        $("#result0").html("PLAYER WINS");
+        $("#result1").html("HOUSE LOSES");
+    }
+    //document.getElementById("result1").innerHTML = 'HOUSE LOSES!'
+    checkScore();
+}
+
+function checkScore() {
+    if (points0 <= 21 && points1 <= 21) {
+        if (points0 > points1) {
+            $("#result0").html("PLAYER WINS");
+            $("#result1").html("HOUSE LOSES");
+        } else if ((points1 > points0)) {
+            $("#result0").html("PLAYER LOSES");
+            $("#result1").html("HOUSE WINS");
+        } else if (points0 === points1) {
+            $("#result0").html("DRAW");
+            $("#result1").html("DRAW");
+        }
+
+    }
+
+
+}
+
+function aceCheck() {
 
 }
